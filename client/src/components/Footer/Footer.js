d5938c711101369f5af50226e2aff36e7e50bf6a
wrunion <winterrunion@gmail.com>
2020-08-23 14:07:15 -0700

    Add semantic markup to footer.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,38 +2,50 @@ import React from "react";
 import * as macLogo from "../../images/mac-logo-horizontal-sm.png";
 import "./Footer.css";
 
+/* QUESTIONS: */ 
+
+/* CHANGES: Added semantic <footer> and <address> tags to improve accessibility. */
+
+/* NOTES: I left existing divs in for now since they're referenced by the stylesheet */
+
 const Footer = (props) => {
   return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="revision-date">
-          Last Update: <br />
-          {props.revisionDate}
-        </div>
-        <div className="spacer" />
-        <div className="footer-item mac-logo">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://mappingaction.org/"
-          >
-            <img src={macLogo} alt="NODE-logo" />
-          </a>
-        </div>
-        <div className="footer-item node-logo">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://opendata.imspdx.org/"
-          >
-            <img
-              src="https://opendata.imspdx.org/uploads/admin/2018-02-28-230321.180610WhiteNODEwlogo.png"
-              alt="NODE-logo"
-            />
-          </a>
+    <footer>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="revision-date">
+            Last Update: <br />
+            {props.revisionDate}
+          </div>
+          <div className="spacer" />
+          <div className="footer-item mac-logo">
+          <address>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://mappingaction.org/"
+            >
+              <img src={macLogo} alt="MAC-logo" />
+            </a>
+            </address>
+          </div>
+          <div className="footer-item node-logo">
+          <address>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://opendata.imspdx.org/"
+            >
+              <img
+                src="https://opendata.imspdx.org/uploads/admin/2018-02-28-230321.180610WhiteNODEwlogo.png"
+                alt="NODE-logo"
+              />
+            </a>
+            </address>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
