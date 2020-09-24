bfdba93ab1c8e067e1064521863cb9ffbaf55b51
wrunion <winterrunion@gmail.com>
2020-09-24 12:46:03 -0700

    Add CSS for responsive emergency button.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import IconSelector from './IconSelector';
 import SearchBar from './SearchBar';
-import './../../css/Home.css';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import EmergencyButtonSmall from './EmergencyButton';
+import BigRedButton from './../../images/emergencyResourcesButton.png';
 
 const BasicInstructions = props => {
   return (
@@ -13,6 +16,9 @@ const BasicInstructions = props => {
     </div>
   );
 };
+
+//784px breakpoint and below - custom CSS
+//above that: the image
 
 const Home = props => {
 
@@ -24,8 +30,16 @@ const Home = props => {
         <div className="home-container">
           <div className="basic-search-container">
             <BasicInstructions />
+              {/* extra div so the Link tag doesn't mess with flexbox spacing */}
+              {/* <div className='emergency-button'>
+                <Link to="results?search=Fire%20Assistance%20Resources">
+                  <img  
+                    src={BigRedButton} 
+                    alt="Click for Emergency Wildfire Resources" />
+                </Link>
+            </div> */}
+            <EmergencyButtonSmall />
             <SearchBar
-              //"Search for a Service..."
               nodeData={nodeData}
               searchData={searchData}
             />
