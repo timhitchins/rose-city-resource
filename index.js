f963fac045db01f43bed72989688aa71e479d1ea
wrunion <winterrunion@gmail.com>
2020-09-30 17:57:35 -0700

    add API, work in progress.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require("./routes/listings")(app);
 require("./routes/phone")(app);
 
 //production boilerplate
-//if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   //make sure express serves up the corret assests
   //like main.js
   app.use(express.static("client/build"));
@@ -24,11 +24,10 @@ require("./routes/phone")(app);
   //this is the catch all code
   const path = require("path");
   app.get("*", (req, res) => {
-    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    res.send("Hello world");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-//}
+}
 
 //heroku dynamic port binding
-const PORT = process.env.PORT || 5100;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
