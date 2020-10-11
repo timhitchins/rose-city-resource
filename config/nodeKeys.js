f8a062f4215e6e93efaebc228132157552128b9a
Kent McKinney <kent.p.mckinney@gmail.com>
2020-10-10 21:11:52 -0700

    refactor code to be more production ready (still WIP)

--- config/nodeKeys.js
+++ config/nodeKeys.js
@@ -4,11 +4,15 @@ if (process.env.NODE_ENV === "production") {
     NODE_PACKAGE_ID: "e9c55b2c-4019-463e-8efa-622f23221402",
     NODE_LISTING_RESOURCE: "9be4623a-3c01-4b2d-9c7b-567a41abbc1c",
     NODE_PHONE_RESOURCE: "2f66ad5d-5066-49d8-846a-6751cfd23863",
+    /* Heroku creates an environment variable called DATABASE_URL for postgres which is subject to change at any time by Heroku */
+    PG_CONNECTION_STRING: process.env.DATABASE_URL
   };
 } else {
   module.exports = {
     NODE_PACKAGE_ID: "592c18db-efa6-44c6-8477-4ffa4103ba94",
     NODE_LISTING_RESOURCE: "61cee891-7d0f-4ebe-b8ea-c0c8d6cb27e7",
     NODE_PHONE_RESOURCE: "4407461b-e99d-4d8e-8a44-18483aa8d13c",
+    /* The local development postgres connection string requires manually adding environment variables to the dev machine */
+    PG_CONNECTION_STRING: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
   };
 }
--- routes/listings.js
+++ routes/listings.js
@@ -1,25 +1,12 @@
 const keys = require("../config/nodeKeys")
 const { Client } = require('pg');
 
-/*
-  ISSUE-10 WORK IN PROGRESS
-  TODO: this comment can be removed once no longer needed
-  Original data source (Northwest Open Data Exchange) which returns JSON:
-  https://opendata.imspdx.org/api/3/action/datastore_search_sql?sql=SELECT * from "61cee891-7d0f-4ebe-b8ea-c0c8d6cb27e7"
-*/
-
 module.exports = (app) => {
   app.get("/api/listings-resource", async (req, res) => {
 
-    // Source the connection string from environment variables
-    // Heroku updates these variables when it makes changes (and this information WILL change)
-    // TODO: we can probably just use process.env.DATABASE_URL
-    const connectionString = process.env.DATABASE_URL;
-    //const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
     // Connect to the database
     let client = new Client({
-      connectionString: connectionString, ssl: { rejectUnauthorized: false }
+      connectionString: PG_CONNECTION_STRING, ssl: { rejectUnauthorized: false }
     });
     await client.connect();
 
@@ -43,6 +30,7 @@ module.exports = (app) => {
       // Return JSON to the client
       await res.json(json);
       await client.end();
+      
     });
   });
 };
