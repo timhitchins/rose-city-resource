5572bbaa974f9645b2266051298facf4179779c5
Kent McKinney <kentmckinney@Kents-MacBook-Air.local>
2021-04-04 16:11:12 -0700

    add compression and cache-headers for production

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const compression = require("compression");
 const path = require("path");
 const { Pool } = require('pg');
 const app = express();
+app.disable('x-powered-by');
 
 /* Determine whether the Node.js environment is development or production */
 const isProdEnvironment = process.env.NODE_ENV === "production";
@@ -37,7 +38,15 @@ function shouldCompress (req, res) {
 app.use(cors());
 app.use(helmet.hidePoweredBy({ setTo: 'Blood, Sweat and Tears' }));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(function(req, res, next) {
+  /* Add Cache-Control headers to all requests */
+  const expireAfterMinutes = 60;
+  const cacheControlHeaderValue = isProdEnvironment
+    ? `public, max-age=${expireAfterMinutes/2 * 60}, stale-while-revalidate=${expireAfterMinutes/2 * 60}`
+    : `no-cache`
+  res.header('Cache-Control', cacheControlHeaderValue);
+  next();
+});
 
 /* Configure view templates, which form the HTML part of the admin and login pages */
 app.set("view engine", "ejs");
@@ -63,7 +72,16 @@ testDatabaseQuery();
 
 /* Default handler for requests not handled by one of the above routes */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, './client/build')), {
+    etag: true,
+    lastModified: true,
+    setHeaders: (res, path) => {
+      if (path.endsWith('.html'))
+        res.setHeader('Cache-Control', 'no-cache'); /* ALWAYS re-validate HTML files! */
+      else
+        res.header('Cache-Control', `max-age=31536000`); /* Aggressively cache other static content */
+      }
+  });
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
