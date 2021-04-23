4fba3c4874e7241551e9fa5f9b5c942316de6f3a
Kent McKinney <kent.p.mckinney@gmail.com>
2021-04-22 23:00:50 -0700

    fix bug in index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ testDatabaseQuery();
 
 /* Default handler for requests not handled by one of the above routes */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './client/build')), {
+  app.use(express.static(path.join(__dirname, '/client/build'), {
     etag: true,
     lastModified: true,
     setHeaders: (res, path) => {
@@ -80,11 +80,20 @@ if (process.env.NODE_ENV === "production") {
       else
         res.header('Cache-Control', `max-age=31536000`); /* Aggressively cache other static content */
       }
-  });
+    })
+  );
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+/* Default handler for requests not handled by one of the above routes */
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
