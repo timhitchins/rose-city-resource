a9fffcc8398ab36371498ccfa83e5f2e4f887750
wrunion <winterrunion@gmail.com>
2021-06-23 00:37:23 -0700

    Refactor passport config for clarity and security.

--- backend/initializePassport.js
+++ backend/initializePassport.js
@@ -1,57 +1,52 @@
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require ('bcrypt')
 
 async function initialize(passport, pool) {
 
   const authenticateUser = async (email, password, done) => {
-    await pool.query(
-      `SELECT * FROM production_user WHERE email = $1`, [email],
-      async (err, results) => {
-        if (err) { throw err; }
-        if (results.rows.length > 0) {
-          const user = results.rows[0];
-          await bcrypt.compare(password, user.password, async (err, isMatch) => {
-            if (err) {
-              console.error(err);
-            }
-            if (isMatch) {
-              return await done(null, user);
-            } else {
-              //password is incorrect
-              return await done(null, false, { message: "Password is incorrect" });
-            }
-          });
-        } else {
-          /* CHECK THIS. doesn't seem to be reaching this far. */
-          return await done(null, false, {
-            message: "No user with that email address"
-          });
-        }
+    try {
+      // Query the DB using the email address entered on the login screen
+      const queryResult = await pool.query(
+        `SELECT * FROM production_user WHERE email = $1`, [email])
+
+        if (!queryResult || !queryResult.rows.length) {
+        // tell passport there was no error ("null")
+        // and that the user doesn't exist ("false")
+        return done(null, false, {
+          message: "No user with that email address"
+        })
+      } else if (queryResult && queryResult.rows.length > 0) {
+        // email is registered
+        const user = queryResult.rows[0]
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) { throw err }
+
+          if (isMatch) {
+
+            return done(null, user);
+          
+          } else {
+            // password is incorrect
+            return done(null, false, { message: 'Password is incorrect' });
+          }
+        });
+      } else {
+        return done(null, false, { message: 'Invalid login credentials' })
       }
-    );
+    } catch (error) {
+      return done(error);
+    }
   };
 
   passport.use(
-    new LocalStrategy(
-      { usernameField: "email", passwordField: "password", roleField: 'role' },
+    new LocalStrategy({ 
+      usernameField: "email", 
+      passwordField: "password"
+      },
       authenticateUser 
     )
   );
-
-  // passport.use(new LocalStrategy({
-  //   usernameField: "email", passwordField: "password", roleField: 'role' , passReqToCallback: true,
-  // },
-  //   function(username, password, role, done) {
-  //     User.findOne({ username: username }, function (err, user) {
-  //       if (err) { return done(err); }
-  //       if (!user) { return done(null, false); }
-  //       if (!user.verifyPassword(password)) { return done(null, false); }
-  //       return done(null, user);
-  //     });
-  //   }
-  // ));
-
-
 
 
   // Stores user details inside session. serializeUser determines which data of the user
@@ -62,13 +57,22 @@ async function initialize(passport, pool) {
 
   // In deserializeUser that key is matched with the in memory array / database or any data resource.
   // The fetched object is attached to the request object as req.user
-
-  passport.deserializeUser(async (id, done) => {
-    await pool.query(`SELECT * FROM production_user WHERE id = $1`, [id], async (err, results) => {
+  passport.deserializeUser((userId, done) => {
+    pool.query(`SELECT * FROM production_user WHERE id = $1`, [userId],  (err, results) => {
       if (err) {
-        return await done(err);
+        return done(err);
       }
-      return await done(null, results.rows[0]);
+      const user = results.rows[0];
+      // Explicitly declare which fields we want to pass to passport
+      // The password has been left out of this object
+      // for security
+      const sessionUser = {
+        id: user.id,
+        name: user.name, 
+        email: user.email,
+        role: user.role
+      }
+      return done(null, sessionUser);
     });
   });
 }
