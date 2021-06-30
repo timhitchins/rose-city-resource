ebf6e456342890e528898dc12c5367ffb5ff2d04
kentpmckinney <kentpmckinney@users.noreply.github.com>
2021-06-30 12:19:07 -0700

    fix banner not updating

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -193,45 +193,46 @@ module.exports = (app, pool) => {
   });
 
   /* Set the Site Banner */
-  app.post('/admin/set-site-banner', userIsAdmin, (req, res) => {
-    const setSiteBanner = async () => {
-      try {
-        const { content, isEnabled } = req.body;
-        const cleanContent = sanitizeHtml(content, {
-          allowedTags: [
-            "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
-            "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
-            "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
-            "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
-            "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
-            "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
-            "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "img"
-          ],
-          disallowedTagsMode: 'discard',
-          allowedAttributes: {
-            a: [ 'href', 'name', 'target' ],
-            img: [ 'src' ],
-            div: [ 'style', 'class' ],
-            span: [ 'style', 'class' ],
-            i: [ 'style', 'class']
-          },
-          selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta' ],
-          allowedSchemes: [ 'http', 'https', 'ftp', 'mailto', 'tel' ],
-          allowedSchemesByTag: {},
-          allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
-          allowProtocolRelative: true,
-          enforceHtmlBoundary: false
-        });
-        await pool.query('SELECT set_site_banner($1, $2);',
-          [cleanContent, isEnabled === true]);
-        res.sendStatus(201);
-      } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-      }
+  app.post('/admin/set-site-banner', userIsAdmin, async (req, res, next) => {
+
+    try {
+      const { content, isEnabled } = req.body;
+      const cleanContent = sanitizeHtml(content, {
+        allowedTags: [
+          "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
+          "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
+          "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
+          "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+          "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+          "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+          "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "img"
+        ],
+        disallowedTagsMode: 'discard',
+        allowedAttributes: {
+          a: [ 'href', 'name', 'target' ],
+          img: [ 'src' ],
+          div: [ 'style', 'class' ],
+          span: [ 'style', 'class' ],
+          i: [ 'style', 'class']
+        },
+        selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta' ],
+        allowedSchemes: [ 'http', 'https', 'ftp', 'mailto', 'tel' ],
+        allowedSchemesByTag: {},
+        allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
+        allowProtocolRelative: true,
+        enforceHtmlBoundary: false
+      });
+
+      await pool.query('SELECT set_site_banner($1, $2);',
+        [cleanContent, isEnabled === true]);
+      const successString = 'Created'
+      res.json(JSON.stringify(successString))
+
+    } catch (err) {
+      console.error(err);
+      return next(err)
     }
 
-    setSiteBanner()
   });
 
   /* Banner */
