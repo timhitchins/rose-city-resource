2afeb0db4e7d9902b4fa10a9707f0cb49d1bffa5
wrunion <winterrunion@gmail.com>
2021-10-03 02:27:42 -0700

    Add constants file to keep config cleaner.

---
+++ config/constants.js
@@ -0,0 +1,27 @@
+module.exports = {
+  SANITIZE_HTML_OPTIONS: {
+    allowedTags: [
+      "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
+      "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
+      "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
+      "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+      "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+      "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+      "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "img"
+    ],
+    disallowedTagsMode: 'discard',
+    allowedAttributes: {
+      a: [ 'href', 'name', 'target' ],
+      img: [ 'src' ],
+      div: [ 'style', 'class' ],
+      span: [ 'style', 'class' ],
+      i: [ 'style', 'class']
+    },
+    selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta' ],
+    allowedSchemes: [ 'http', 'https', 'ftp', 'mailto', 'tel' ],
+    allowedSchemesByTag: {},
+    allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
+    allowProtocolRelative: true,
+    enforceHtmlBoundary: false
+  }
+}
