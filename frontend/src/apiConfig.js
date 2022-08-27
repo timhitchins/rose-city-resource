2a954b8cb43dd1b0bf4daad57ca9ad8e01cffd81
Winter Runion <winterrunion@gmail.com>
2022-08-27 10:29:54 -0700

    Add api config to the FE.

---
+++ frontend/src/apiConfig.js
@@ -0,0 +1,11 @@
+export const PRODUCTION_CONFIG = {
+  META_URI: "/api/meta-information",
+  QUERY: "/api/query",
+  QUERY_STAGING: "/api/query-staging",
+};
+
+export const CONFIG = {
+  META_URI: "https://rosecityresource.streetroots.org/api/meta-information",
+  QUERY: "https://rosecityresource.streetroots.org/api/query",
+  QUERY_STAGING: "https://rosecityresource.streetroots.org/api/query-staging",
+};
